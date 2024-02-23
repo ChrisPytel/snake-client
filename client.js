2f998dfc16b8586cba6b55676b9e92b80d971181
ChrisPytel <chris_pytel@hotmail.com>
2024-02-23 17:46:28 -0500

    client.js A1 - send connection message to server and username initials

--- client.js
+++ client.js
@@ -1,18 +1,29 @@
-//"client.js added - "
+//"client.js A1 - send connection message to server and username initials"
 
 const net = require("net");
+let threeInitials = "CHR";
 
 // establishes a connection with the game server
-const connect = function () {
+const connectToServer = function () {
   const conn = net.createConnection({
     host: "172.28.207.93",    // IP address here,
     port: "50541"            // PORT number here,
   });
   conn.setEncoding("utf8");
-  return conn;
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to server");
+    conn.write("Hello from client!");
+    conn.write(`Name: ${threeInitials}`)
+  });
+
+
+  //return conn; //commented out the return call to see what happens
 };
 
 
-module.exports = connect; //exports our connect function to communicate to play.js
+module.exports = { //exports our connect function to communicate to play.js
+  connectToServer
+}; 
 
-console.log();
+console.log(connectToServer);
