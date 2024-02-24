ef7f13f15eeef1a27e08cc52d797fee34bf34af3
ChrisPytel <chris_pytel@hotmail.com>
2024-02-24 00:26:43 -0500

    input.js A1 - connection object passed in

--- input.js
+++ input.js
@@ -1,8 +1,12 @@
-//input.js added - brought in functions from play.js here, module.exports setupInput function"
+//"input.js A1 - connection object passed in"
+
+// Stores the active TCP connection object.
+let connection;
 
 
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,11 +15,23 @@ const setupInput = function () {
   return stdin;
 };
 
-
 //used for checking our key inputs
 const handleUserInput = function(keyPress) {  
+  if (keyPress === "w") {
+    console.log(`Moved up`);
+    connection.write("Move: up");
+  }   
   if (keyPress === "a") {
-    console.log(`test`);
+    console.log(`Moved left`);
+    connection.write("Move: left");
+  }  
+  if (keyPress === "s") {
+    console.log(`Moved down`);
+    connection.write("Move: down");
+  }   
+  if (keyPress === "d") {
+    console.log(`Moved right`);
+    connection.write("Move: right");
   }   
   if (keyPress === '\u0003') {
     process.exit();
