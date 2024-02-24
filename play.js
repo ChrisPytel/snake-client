afb7631f3f89890298e6b6983075a2512f457e6c
ChrisPytel <chris_pytel@hotmail.com>
2024-02-24 00:27:57 -0500

    play.js A3 - returning object from connection module and passed into input controller

--- play.js
+++ play.js
@@ -1,15 +1,17 @@
-//"play.js A2 - import input module and changed to object destructuring assignment"
-//this is the client side of snake, not the server side
+//"play.js A3 - returning object from connection module and passed into input controller"
+//this is the client side of snake
 
 // const connect = require("./client.js");
 // const input = require("./input.js");    //Refactored below
 
-const { connect } = require("./client.js");
-const { input } = require("./input.js"); //
+
+const { connect } = require("./client");
+const { setupInput } = require("./input"); //
 //Note: ^ This is called object destructuring assignment.
 //Rather than importing the entire file contents, it only imports the specific elements from that module we exported
 //Allows for directly calling the function --> setupInput(); instead of doing input.setupInput();
 
 console.log("Connecting ...");
-connect();
-setupInput();
+
+let conn = connect();
+setupInput(conn);
